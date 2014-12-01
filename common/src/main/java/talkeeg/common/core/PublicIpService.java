82e7fb583c436c7068178aeaa103f8722385c56f
wayerr <radiofun@ya.ru>
2014-12-01 10:27:18 +0300

    improve PublicIpService

--- common/src/main/java/talkeeg/common/core/PublicIpService.java
+++ common/src/main/java/talkeeg/common/core/PublicIpService.java
@@ -30,11 +30,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -42,25 +43,51 @@ import java.util.logging.Logger;
  * retrieve public (external) ip for specified local ip, <p/>
  * we don`t have need this service after implementing external server
  *
- * TODO we must add watchdog which will be kill tasks with hanged requests
- *
  * Created by wayerr on 30.11.14.
  */
 final class PublicIpService implements Function<InetAddress, InetAddress>, Closeable {
     private static final Logger LOG = Logger.getLogger(PublicIpService.class.getName());
 
-    private class Service {
+    private final class Service implements Runnable {
         private final URL url;
-        private long timeout;
+        private long started;
+        private volatile String ip;
+        private boolean end;
 
         private Service(String uriString) throws MalformedURLException {
             this.url = new URL(uriString);
         }
 
-        String getIp() throws IOException {
-            final HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-            try(InputStreamReader r = new InputStreamReader(connection.getInputStream())) {
-                return CharStreams.toString(r);
+        @Override
+        public void run() {
+            synchronized(this) {
+                this.started = System.currentTimeMillis();
+            }
+            try {
+                final HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+                try(InputStreamReader r = new InputStreamReader(connection.getInputStream())) {
+                    this.ip = CharStreams.toString(r);
+                }
+            } catch(IOException e) {
+                //we don`t need full stack trace
+                LOG.log(Level.SEVERE, "error with " + url + ": " + e.toString());
+            } finally {
+                synchronized(this) {
+                   this.end = true;
+                }
+            }
+        }
+
+        /**
+         * time after execution start, if already executed, then return -1
+         * @return
+         */
+        long getExecutionTime() {
+            synchronized(this) {
+                if(this.end) {
+                    return -1;
+                }
+                return this.started;
             }
         }
     }
@@ -74,13 +101,41 @@ final class PublicIpService implements Function<InetAddress, InetAddress>, Close
     private final Runnable resolveFastestService = new Runnable() {
         @Override
         public void run() {
-            final List<Service> sorted = new ArrayList<>();
-            for(String serviceUrlString : knowedServices) {
-                try {
-                    sorted.add(new Service(serviceUrlString));
-                } catch(MalformedURLException e) {
-                    LOG.log(Level.WARNING, "for string: " + serviceUrlString, e);
+            final List<Service> sorted = Collections.synchronizedList(new ArrayList<>());
+            final List<Future<?>> tasks = new ArrayList<>();
+            final Lock lock = new ReentrantLock();
+            final Condition condition = lock.newCondition();
+            try {
+                for(final String serviceUrlString : knowedServices) {
+                    try {
+                        final Service service = new Service(serviceUrlString);
+                        tasks.add(executor.submit(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    service.run();
+                                    sorted.add(service);
+                                } catch(Exception e) {
+                                    //errors in this is a usual thing
+                                } finally {
+                                    condition.signalAll();
+                                }
+                            }
+                        }));
+                    } catch(MalformedURLException e) {
+                        LOG.log(Level.SEVERE, "onr url " + serviceUrlString, e);
+                    }
+                    //yes, here we can use scheduled thread pool, but for what?
+                    Thread.sleep(100);
                 }
+                //wait first executed thread
+                condition.await(1, TimeUnit.MINUTES);
+                //stop all other tasks
+                for(Future<?> future: tasks) {
+                    future.cancel(true);
+                }
+            } catch(InterruptedException e) {
+                //what we do there&
             }
             synchronized(services) {
                 services.clear();
@@ -106,35 +161,25 @@ final class PublicIpService implements Function<InetAddress, InetAddress>, Close
 
     @Override
     public InetAddress apply(InetAddress input) {
-        String externalIpString = null;
-        while(externalIpString == null) {
-            Service service;
-            synchronized(services) {
-                if(services.size() > 0) {
-                    service = services.get(0);
-                } else {
-                    service = null;
-                }
-            }
-            if(service == null) {
-                //maybe we need to wait before services was reloaded?
-                reloadServices();
-                break;
-            }
-            try {
-                externalIpString = service.getIp();
-            } catch(IOException e) {
-                //service was broken, disable service and try next service
-                LOG.log(Level.SEVERE, "service " + service.url, e);
-                synchronized(services) {
-                    services.remove(service);
-                }
+        Service service;
+        synchronized(services) {
+            if(services.size() > 0) {
+                service = services.get(0);
+            } else {
+                service = null;
             }
         }
-        try {
-            return InetAddress.getByName(externalIpString);
-        } catch(UnknownHostException e) {
-            LOG.log(Level.SEVERE, "for external ip: " + externalIpString, e);
+        if(service == null) {
+            //maybe we need to wait before services was reloaded?
+            reloadServices();
+        } else {
+
+            String externalIpString = service.ip;
+            try {
+                return InetAddress.getByName(externalIpString);
+            } catch(UnknownHostException e) {
+                LOG.log(Level.SEVERE, "for external ip: " + externalIpString, e);
+            }
         }
         return null;
     }
