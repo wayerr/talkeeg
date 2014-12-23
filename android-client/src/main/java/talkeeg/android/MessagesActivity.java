6eb5ffd53afa5f3bc05e0b244569ae5421a082c1
wayerr <radiofun@ya.ru>
2014-12-23 15:13:36 +0300

    + MessagesActivity support for history log

--- android-client/src/main/java/talkeeg/android/MessagesActivity.java
+++ android-client/src/main/java/talkeeg/android/MessagesActivity.java
@@ -20,14 +20,37 @@
 package talkeeg.android;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import talkeeg.bf.BinaryData;
+import talkeeg.bf.Int128;
+import talkeeg.common.core.DataService;
+import talkeeg.common.model.Constants;
+import talkeeg.common.model.Data;
+import talkeeg.common.util.Callback;
+import talkeeg.common.util.Closeable;
+import talkeeg.common.util.Closeables;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * activity of messages list
  * Created by wayerr on 22.12.14.
  */
 public final class MessagesActivity extends Activity {
+
+    private DataService dataService;
+    private Int128 clientId;
+    private MessagesListAdapter listAdapter;
 
     /**
      * Called when the activity is first created.
@@ -41,6 +64,27 @@ public final class MessagesActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.messages_activity);
 
+
+        this.dataService = App.get(DataService.class);
+        this.listAdapter = new MessagesListAdapter(this, R.layout.messages_history_view, dataService);
+
+
+        ListView messagesList = (ListView)findViewById(R.id.messagesListView);
+        messagesList.setAdapter(this.listAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        Closeables.close(this.listAdapter);
+        super.onDestroy();
+    }
+
+    public void sendMessageAction(View view) {
+        if(this.clientId == null) {
+            return;
+        }
+        BinaryData data = new BinaryData("тест уникода".getBytes());
+        this.dataService.push(this.clientId, Data.buidler().action(Constants.DATA_ACTION_CHAT).data(data).build());
     }
 
     @Override
@@ -48,5 +92,75 @@ public final class MessagesActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    private static final class MessagesListAdapter extends BaseAdapter implements Closeable {
+
+        private final DataService service;
+        private final LayoutInflater inflater;
+        private final int resource;
+        private final List<Data> history = new ArrayList<>();
+        private final Callback<Data> chatCallback = new Callback<Data>() {
+            @Override
+            public void call(Data value) {
+                history.add(value);
+            }
+        };
+        private final Closeable callbackEnd;
+
+        private MessagesListAdapter(Context context, int resource, DataService service) {
+            this.service = service;
+            this.resource = resource;
+            this.inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            this.callbackEnd = this.service.addHandler(Constants.DATA_ACTION_CHAT, chatCallback);
+        }
+
+        @Override
+        public int getCount() {
+            return this.history.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return this.history.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View itemView;
+            if(convertView == null) {
+                itemView = this.inflater.inflate(this.resource, parent, false);
+            } else {
+                itemView = convertView;
+            }
+            TextView sourceView = (TextView)itemView.findViewById(R.id.messagesListItemSource);
+            TextView dataView  = (TextView)itemView.findViewById(R.id.messagesListItemData);
+
+            Data data = null;
+            if(this.history.size() > position) {
+                data = this.history.get(position);
+            }
+            if(data == null) {
+                sourceView.setText(null);
+                dataView.setText(null);
+            } else {
+                sourceView.setText(data.getAction());
+                final BinaryData binaryData = data.getData();
+                dataView.setText(new String(binaryData.getData()));
+            }
+
+            return itemView;
+        }
+
+        @Override
+        public void close() {
+            Closeables.close(this.callbackEnd);
+        }
     }
 }
